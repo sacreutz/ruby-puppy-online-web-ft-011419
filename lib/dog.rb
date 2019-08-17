3895d89e210043889eb54f6a50d5fb48d8079aa1
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-17 17:28:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -17,6 +17,10 @@ class Dog
     @@all.each do |dog| 
       puts dog.name
     end
+  end
+  
+  def save 
+    @@all << self
   end
 
 
